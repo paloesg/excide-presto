7169bdb37e844268c63530763146a8e8178d80d5
Vicky Prahastra <vicky@excide.co>
2018-10-17 21:02:55 +0700

    Index page for admin dashboard

--- app/controllers/dashboard/home_controller.rb
+++ app/controllers/dashboard/home_controller.rb
@@ -1,2 +1,6 @@
 class Dashboard::HomeController < ApplicationController
+layout 'dashboard/application'
+
+  def show
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,7 @@ Rails.application.routes.draw do
   # the default of "spree".
   mount Spree::Core::Engine, at: '/'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :dashboard do
+    root 'home#show'
+  end
 end
