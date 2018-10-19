56461b4d729277113215a2ea1b70baef6a067be2
Vicky Prahastra <vicky@excide.co>
2018-10-19 11:33:38 +0700

    Create the edit order page of dashboard

--- app/controllers/dashboard/orders_controller.rb
+++ app/controllers/dashboard/orders_controller.rb
@@ -1,2 +1,7 @@
 class Dashboard::OrdersController < ApplicationController
+layout 'dashboard/application'
+
+  def edit
+    @order = Spree::Order.includes(:adjustments).find_by!(number: params[:id])
+  end
 end
