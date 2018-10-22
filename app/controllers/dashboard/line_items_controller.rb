a77f1fcab574c93c03247515f505cb3a92af02e2
Vicky Prahastra <vicky@excide.co>
2018-10-22 15:22:47 +0700

    Redirect back after reject an item of order

--- app/controllers/dashboard/line_items_controller.rb
+++ app/controllers/dashboard/line_items_controller.rb
@@ -1,7 +1,9 @@
 class Dashboard::LineItemsController < ApplicationController
 
   def destroy
+    @order = Spree::Order.includes(:adjustments).find_by!(number: params[:order_id])
     @line_item = Spree::LineItem.find(params[:id])
     @line_item.destroy
+    redirect_to edit_dashboard_order_path(order_id: @order.number)
   end
 end
