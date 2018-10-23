207d06959abea8c5491039d8809ddbce13d2df6b
Vicky Prahastra <vicky@excide.co>
2018-10-23 10:01:15 +0700

    Refactor, set order

--- app/controllers/dashboard/orders_controller.rb
+++ app/controllers/dashboard/orders_controller.rb
@@ -1,19 +1,23 @@
 class Dashboard::OrdersController < ApplicationController
+before_action :set_order
 layout 'dashboard/application'
 
   def edit
-    @order = Spree::Order.includes(:adjustments).find_by!(number: params[:order_id])
   end
 
   def approve
-    @order = Spree::Order.includes(:adjustments).find_by!(number: params[:order_id])
     @order.approved_by(spree_current_user)
     redirect_to dashboard_root_path
   end
 
   def cancel
-    @order = Spree::Order.includes(:adjustments).find_by!(number: params[:order_id])
     @order.canceled_by(spree_current_user)
     redirect_to dashboard_root_path
+  end
+
+  private
+
+  def set_order
+    @order = Spree::Order.includes(:adjustments).find_by!(number: params[:order_id])
   end
 end
