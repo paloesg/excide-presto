3754fc5c4dfe6b308ac0fd4b3ac0cec4226b4d24
Vicky Prahastra <vicky@excide.co>
2018-10-25 11:44:25 +0700

    Display product on bootstrap modal

--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,5 @@ Rails.application.routes.draw do
   # the default of "spree".
   mount Spree::Core::Engine, at: '/'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/products-load/:id', to: 'spree/products#show_modal'
 end
