82f973a68a4e185305615f24701aff605494eb96
Vicky Prahastra <vicky@excide.co>
2018-10-26 10:43:14 +0700

    Display brand of product on popover

--- app/controllers/spree/products_controller_decorator.rb
+++ app/controllers/spree/products_controller_decorator.rb
@@ -1,6 +1,7 @@
 Spree::ProductsController.class_eval do
   def show_modal
     load_product
+    @brand = @product.taxons.select { |taxon| taxon.taxonomy.name == 'Brands' }.first
     render :layout => false
   end
 end
