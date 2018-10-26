b0346fc5d526550ec6ebcc49c3601fe35ecb0fc6
Vicky Prahastra <vicky@excide.co>
2018-10-26 10:53:45 +0700

    Refactor display brands of product

--- app/controllers/spree/products_controller_decorator.rb
+++ app/controllers/spree/products_controller_decorator.rb
@@ -1,7 +1,6 @@
 Spree::ProductsController.class_eval do
   def show_modal
     load_product
-    @brand = @product.taxons.select { |taxon| taxon.taxonomy.name == 'Brands' }.first
     render :layout => false
   end
 end
