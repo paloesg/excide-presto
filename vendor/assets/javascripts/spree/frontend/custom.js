5680056c188c0dc8ae129c1fbbd2e4674e75df2d
Vicky Prahastra <vicky@excide.co>
2018-10-29 11:56:58 +0700

    Move javascript to js files

---
+++ vendor/assets/javascripts/spree/frontend/custom.js
@@ -0,0 +1,28 @@
+function showProduct(url) {
+  // Load Product Content
+  $("#modalContent").load(url, function() {
+    // Load Js for switching the product main image by hovering the thumbnails
+    var thumbnails = $("#product-images").next();
+    ($("#main-image")).data("selectedThumb", ($("#main-image img")).attr("src"));
+    thumbnails.find("li").eq(0).addClass("selected");
+    thumbnails.find("a").on("click", function (event) {
+      ($("#main-image")).data("selectedThumb", ($(event.currentTarget)).attr("href"));
+      ($("#main-image")).data("selectedThumbId", ($(event.currentTarget)).parent().attr("id"));
+      thumbnails.find("li").removeClass("selected");
+      ($(event.currentTarget)).parent("li").addClass("selected");
+      return false;
+    });
+    thumbnails.find("li").on("mouseenter", function (event) {
+      return ($("#main-image img")).attr("src", ($(event.currentTarget)).find("a").attr("href"));
+    });
+    thumbnails.find("li").on("mouseleave", function (event) {
+      return ($("#main-image img")).attr("src", ($("#main-image")).data("selectedThumb"));
+    });
+  });
+  $('#productContent').on('hidden.bs.modal', function (e) {
+    $('#productContent').find('.modal-body').html('<div class="loader"><div>');
+  })
+}
+$('.button_to').on('ajax:success', function(event, xhr, status, error) {
+  Spree.fetch_cart();
+})
