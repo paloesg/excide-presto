6a51043536a215e80c16dbc7e747a1cf3460601f
Vicky Prahastra <vicky@excide.co>
2018-10-29 10:52:37 +0700

    Add to cart respond JSON

--- app/controllers/spree/orders_controller_decorator.rb
+++ app/controllers/spree/orders_controller_decorator.rb
@@ -1,3 +1,11 @@
 Spree::OrdersController.class_eval do
-  respond_override populate: { html: { success: lambda { redirect_back_or_default(spree.root_path) } } }
+	before_action :set_cart
+  respond_override populate: { html: { success: lambda { render json: set_cart.to_json } } }
+
+	private
+
+  def set_cart
+    @order = current_order
+    @cart = { total: @order[:total], item_count: @order[:item_count] }
+  end
 end
