50bfd85ec4d5514771faa746f814d5665598811d
Vicky Prahastra <vicky@excide.co>
2018-10-29 11:30:48 +0700

    Update nav menu cart status using ajax

--- app/controllers/spree/orders_controller_decorator.rb
+++ app/controllers/spree/orders_controller_decorator.rb
@@ -1,11 +1,10 @@
 Spree::OrdersController.class_eval do
-	before_action :set_cart
-  respond_override populate: { html: { success: lambda { render json: set_cart.to_json } } }
+  respond_override populate: { html: { success: lambda { render json: cart } } }
 
 	private
 
-  def set_cart
-    @order = current_order
-    @cart = { total: @order[:total], item_count: @order[:item_count] }
+  def cart
+    order = current_order
+    @cart = { total: order[:total], item_count: order[:item_count] }
   end
 end
