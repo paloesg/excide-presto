203fbb346dbce93115f42ffa206caf2d70583b48
Vicky Prahastra <vicky@excide.co>
2018-11-12 14:21:38 +0700

    Override high voltage pages controller and route

--- config/routes.rb
+++ config/routes.rb
@@ -10,4 +10,5 @@ Rails.application.routes.draw do
   mount Spree::Core::Engine, at: '/'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/products-load/:id', to: 'spree/products#show_modal'
+  get '/pages/*id' => 'pages#show', as: :page, format: false
 end
