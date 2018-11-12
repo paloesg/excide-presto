4d94ab92d2d4eb5851be796332b547938245cd52
Vicky Prahastra <vicky@excide.co>
2018-11-12 16:22:14 +0700

    Display sidebar on pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,13 @@
 class PagesController < Spree::BaseController
   include HighVoltage::StaticPage
   include Spree::Core::ControllerHelpers::Order
+  before_action :set_taxonomies
+
   layout 'spree/layouts/spree_application'
+
+  private
+
+  def set_taxonomies
+    @taxonomies = Spree::Taxonomy.includes(root: :children)
+  end
 end
