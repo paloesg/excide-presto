46f9f2c6f2aeec5828b84557b5c4a5f9ea805a50
Vicky Prahastra <vicky@excide.co>
2018-11-13 17:45:28 +0700

    Sort price and name of products

--- app/controllers/spree/products_controller_decorator.rb
+++ app/controllers/spree/products_controller_decorator.rb
@@ -3,4 +3,28 @@ Spree::ProductsController.class_eval do
     load_product
     render :layout => false
   end
+
+  def index
+    @searcher = build_searcher(params.merge(include_images: true))
+    @products = @searcher.retrieve_products
+    @products = sort_products(@products)
+    @products = @products.includes(:possible_promotions) if @products.respond_to?(:includes)
+    @taxonomies = Spree::Taxonomy.includes(root: :children)
+  end
+
+  private
+
+  def sort_products(products)
+    if params[:sort] == 'price' and params[:direction] == 'asc'
+      products.reorder('').send(:ascend_by_master_price)
+    elsif params[:sort] == 'price' and params[:direction] == 'desc'
+      products.reorder('').send(:descend_by_master_price)
+    elsif params[:sort] == 'name' and params[:direction] == 'asc'
+      products.reorder('').send(:ascend_by_name)
+    elsif params[:sort] == 'name' and params[:direction] == 'desc'
+      products.reorder('').send(:descend_by_name)
+    else
+      products
+    end
+  end
 end
