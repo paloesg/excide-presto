7ddb94c43b6078568bfdf9c1242f856aa1e34395
Vicky Prahastra <vicky@excide.co>
2018-11-15 22:29:41 +0700

    Display service requests listing

--- app/controllers/spree/admin/service_requests_controller.rb
+++ app/controllers/spree/admin/service_requests_controller.rb
@@ -2,6 +2,7 @@ module Spree
   module Admin
     class ServiceRequestsController < ResourceController
       def index
+        # @service_requests = Spree::ServiceRequest.all
       end
     end
   end
