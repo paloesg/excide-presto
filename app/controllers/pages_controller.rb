be45d57869397e46acbdf8feff68c8974a3427cc
Vicky Prahastra <vicky@excide.co>
2018-11-16 17:34:11 +0700

    Submit form to create new service request

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,6 +5,14 @@ class PagesController < Spree::BaseController
 
   layout 'spree/layouts/spree_application'
 
+  def create_request
+    @service_request = Spree::ServiceRequest.new
+    @service_request.service_name = params[:id]
+    @service_request.spree_user_id = spree_current_user.id
+    @service_request.fields = params[:fields]
+    @service_request.save
+  end
+
   private
 
   def set_taxonomies
