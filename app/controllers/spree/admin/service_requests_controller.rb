f7766be5df92e91b2329813d6c700d8e90725aed
Vicky Prahastra <vicky@excide.co>
2018-11-16 11:21:06 +0700

    Create show service request page

--- app/controllers/spree/admin/service_requests_controller.rb
+++ app/controllers/spree/admin/service_requests_controller.rb
@@ -2,7 +2,11 @@ module Spree
   module Admin
     class ServiceRequestsController < ResourceController
       def index
-        # @service_requests = Spree::ServiceRequest.all
+        @service_requests = Spree::ServiceRequest.all
+      end
+
+      def show
+        @service_request = Spree::ServiceRequest.find(params[:id])
       end
     end
   end
