2542538aee65c4948c053d3ebeaed35295a71903
Vicky Prahastra <vicky@excide.co>
2018-11-23 12:01:19 +0700

    Fix javascript not loaded after turbolinks load page

--- app/assets/javascripts/components/select_sort_by.js
+++ app/assets/javascripts/components/select_sort_by.js
@@ -1,4 +1,4 @@
-$(document).ready(function () {
+$(document).on('turbolinks:load', function () {
   $('select#sort_by').on('change', function() {
     Turbolinks.visit('//' + location.host + location.pathname + this.value);
   })
