df9a089c53a416e5bddf4beaf5a60dba6fa7405c
Vicky Prahastra <vicky@excide.co>
2018-11-26 09:42:01 +0700

    Sort products by brands name

--- app/controllers/spree/products_controller_decorator.rb
+++ app/controllers/spree/products_controller_decorator.rb
@@ -15,7 +15,11 @@ Spree::ProductsController.class_eval do
   private
 
   def sort_products(products)
-    if params[:sort] == 'price_asc'
+    if params[:sort] == 'brand_asc'
+      products.includes(:taxons).where(spree_taxons: {taxonomy_id: Spree::Taxonomy.find_by(name: 'Brands').id}).order("spree_taxons.name asc")
+    elsif params[:sort] == 'brand_desc'
+      products.includes(:taxons).where(spree_taxons: {taxonomy_id: Spree::Taxonomy.find_by(name: 'Brands').id}).order("spree_taxons.name desc")
+    elsif params[:sort] == 'price_asc'
       products.reorder('').send(:ascend_by_master_price)
     elsif params[:sort] == 'price_desc'
       products.reorder('').send(:descend_by_master_price)
