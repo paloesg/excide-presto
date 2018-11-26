35761eb0f40073634d1686028cd6978d5a03757e
frediansimanjuntak <fredian.pc@gmail.com>
2018-11-26 12:28:56 +0700

    change method price_in in variant model

--- app/models/spree/variant_decorator.rb
+++ app/models/spree/variant_decorator.rb
@@ -1,3 +1,12 @@
 Spree::Variant.class_eval do
+  alias_method :orig_price_in, :price_in
   has_many :product_sales
+
+  def price_in(currency)
+    store = Spree::Store.current
+    current_store_id = store.id
+    product_sale = self.product_sales.find_by(store_id: current_store_id) if self.product_sales.find_by(store_id: current_store_id)
+    return orig_price_in(currency) unless product_sale.present?
+    Spree::Price.new(variant_id: self.id, amount: product_sale.sale_price, currency: currency)
+  end
 end
