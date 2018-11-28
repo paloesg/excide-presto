2e99cfe0d07bc14b5b0ac4285139613efe00d8e5
frediansimanjuntak <fredian.pc@gmail.com>
2018-11-28 16:49:07 +0700

    add helper method sold_out in product

--- app/models/spree/product_decorator.rb
+++ app/models/spree/product_decorator.rb
@@ -2,4 +2,8 @@ Spree::Product.class_eval do
   def brand
     self.taxons.select { |taxon| taxon.taxonomy.name == 'Brands' }.first&.name
   end
+
+  def sold_out?
+    self.total_on_hand == 0 && self.master.is_backorderable? == false
+  end
 end
