db08ac7093c92c314f13642608cb8c7e330b4765
Vicky Prahastra <vicky@excide.co>
2018-11-28 16:12:57 +0700

    Rename class and add styles sorting button

--- app/assets/javascripts/components/select_sort_by.js
+++ app/assets/javascripts/components/select_sort_by.js
@@ -1,5 +1,5 @@
 $(document).on('turbolinks:load', function () {
-  $('select#sort_by').on('change', function() {
+  $('select#sort-by').on('change', function() {
     Turbolinks.visit('//' + location.host + location.pathname + this.value);
   })
 })
