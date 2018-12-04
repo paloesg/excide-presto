d527addf4b04582c7e6d1b0feee80bbb9d313494
Vicky Prahastra <vicky@excide.co>
2018-12-04 18:52:56 +0700

    Custom template sub-categories popover

--- app/assets/javascripts/components/sub_categories_popover.js
+++ app/assets/javascripts/components/sub_categories_popover.js
@@ -1,17 +1,22 @@
 $(document).on('turbolinks:load', function(){
-  $('[data-toggle="popover"]').popover({ trigger: "manual", html: true, container: 'body' })
-    .on("mouseenter", function () {
-      var _this = this;
-      $(this).popover("show");
-      $(".popover").on("mouseleave", function () {
-        $(_this).popover('hide');
-      });
-    }).on("mouseleave", function () {
-      var _this = this;
-      setTimeout(function () {
-        if (!$(".popover:hover").length) {
-          $(_this).popover("hide");
-        }
-      }, 30)
-    })
+  $('[data-toggle="popover"]').popover({
+    trigger: "manual",
+    html: true,
+    container: 'body',
+    animation: false,
+    template: '<div class="popover sub-categories"><div class="popover-content"></div></div>'
+  }).on("mouseenter", function () {
+    var _this = this;
+    $(this).popover("show");
+    $(".popover").on("mouseleave", function () {
+      $(_this).popover('hide');
+    });
+  }).on("mouseleave", function () {
+    var _this = this;
+    setTimeout(function () {
+      if (!$(".popover:hover").length) {
+        $(_this).popover("hide");
+      }
+    }, 30)
+  })
 })
