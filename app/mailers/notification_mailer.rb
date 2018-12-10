06ade6aa4d5319391be2517726fbd70240f5cbe4
Vicky Prahastra <vicky@excide.co>
2018-12-10 13:22:11 +0700

    Send new service request notification to users with role admin

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -3,7 +3,15 @@ class NotificationMailer < ApplicationMailer
   layout 'mailer'
 
   def new_service_request(service_request)
+    @users = Spree::Role.find_by_name('admin').users
     @service_request = service_request
-    mail(to: 'admin@excide.co', subject: '[New Service Request] ' + @service_request.service_name)
+    @users.each do |user|
+      notification_service_request(service_request, user).deliver
+    end
+  end
+
+  def notification_service_request(service_request, user)
+    @service_request = service_request
+    mail(to: user.email, subject: '[New Service Request] ' + @service_request.service_name)
   end
 end
