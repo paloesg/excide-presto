c8dbb0c814f36908164afe5c212ae23e00af2e01
Vicky Prahastra <vicky@excide.co>
2018-12-11 15:10:14 +0700

    Add company validation when create or edit

--- app/models/spree/company.rb
+++ app/models/spree/company.rb
@@ -2,4 +2,7 @@ class Spree::Company < ApplicationRecord
   has_many  :departments
   has_many :users
   has_one :store
+  validates :name, presence: true
+  validates :address, presence: true
+  validates :description, presence: true
 end
