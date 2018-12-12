5ba132a9fa66a381da86233141d1c5684b2f2904
Vicky Prahastra <vicky@excide.co>
2018-12-12 13:09:06 +0700

    Fix display flash message company field of form

--- app/models/spree/department.rb
+++ app/models/spree/department.rb
@@ -2,5 +2,6 @@ class Spree::Department < Spree::Base
   belongs_to :company
   has_many :users
   validates :name, presence: true
+  validates :company, presence: true
   validates :description, presence: true
 end
