b4334dfd3a212515503239be8d95d2725579bf07
Vicky Prahastra <vicky@excide.co>
2018-12-12 08:52:37 +0700

    Create and edit department

--- app/controllers/spree/admin/departments_controller.rb
+++ app/controllers/spree/admin/departments_controller.rb
@@ -1,2 +1,42 @@
-class Spree::Admin::DepartmentsController < ApplicationController
+module Spree
+  module Admin
+    class DepartmentsController < ResourceController
+      def index
+      end
+
+      def new
+        @deparment = Spree::Department.new
+        @companies = Spree::Company.all
+      end
+
+      def create
+        @department = Spree::Department.new(department_params)
+        if @department.save
+          flash[:success] = flash_message_for(@department, :successfully_created)
+          redirect_to admin_departments_path
+        else
+          render :new
+        end
+      end
+
+      def edit
+        @department = Spree::Department.find(params[:id])
+        @companies = Spree::Company.all
+      end
+
+      def update
+        @department = Spree::Department.find(params[:id])
+        if @department.update_attributes(department_params)
+          flash[:success] = Spree.t(:department_updated)
+          redirect_to admin_departments_path
+        else
+          render :edit
+        end
+      end
+
+      def department_params
+        params.require(:department).permit(:name, :company_id, :description)
+      end
+    end
+  end
 end
--- app/models/spree/department.rb
+++ app/models/spree/department.rb
@@ -1,4 +1,6 @@
 class Spree::Department < ApplicationRecord
   belongs_to :company
   has_many :users
+  validates :name, presence: true
+  validates :description, presence: true
 end
