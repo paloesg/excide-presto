c569cc835143e56af980af0537c2fd4354949b34
Vicky Prahastra <vicky@excide.co>
2018-12-15 09:12:52 +0700

    Update display icons homepage

--- app/controllers/spree/home_controller_decorator.rb
+++ app/controllers/spree/home_controller_decorator.rb
@@ -2,6 +2,6 @@ Spree::HomeController.class_eval do
   before_action :set_categories
 
   def set_categories
-    @categories = Spree::Taxonomy.find_by_name('Categories').root.children.take(4)
+    @categories = Spree::Taxonomy.find_by_name('Categories').root.children.take(3)
   end
 end
