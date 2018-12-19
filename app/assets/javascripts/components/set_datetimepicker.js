9920d8d546d891b7e1df08a374d82b8568c1f4db
Vicky Prahastra <vicky@excide.co>
2018-12-19 14:03:12 +0700

    Update fields designs

--- app/assets/javascripts/components/set_datetimepicker.js
+++ app/assets/javascripts/components/set_datetimepicker.js
@@ -1,7 +1,10 @@
 $(function () {
-  $('#CartTimepicker').datetimepicker( {
+  $('.cart-datetime-picker').datetimepicker( {
     stepping: 60,
     enabledHours: [9, 10, 11, 12, 13, 14, 15, 16, 17],
-    format: 'HH:mm'
+    format: 'DD-MM-YYYY HH:mm'
   });
+
+  $('.cart-datetime-picker').val('');
+  $('.cart-datetime-picker').attr("placeholder","DD-MM-YYYY HH:MM");
 });
