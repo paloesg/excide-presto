6e59d1f44184387a849d281151bc019af54973d2
Vicky Prahastra <vicky@excide.co>
2018-12-19 15:51:17 +0700

    Disable past dates

--- app/assets/javascripts/components/set_datetimepicker.js
+++ app/assets/javascripts/components/set_datetimepicker.js
@@ -1,10 +1,12 @@
 $(function () {
+  var date = new Date();
+  date.setDate(date.getDate()+1);
   $('.cart-datetime-picker').datetimepicker( {
     stepping: 60,
     enabledHours: [9, 10, 11, 12, 13, 14, 15, 16, 17],
-    format: 'DD-MM-YYYY HH:mm'
+    format: 'DD-MM-YYYY HH:mm',
+    minDate: date
   });
-
   $('.cart-datetime-picker').val('');
   $('.cart-datetime-picker').attr("placeholder","DD-MM-YYYY HH:MM");
 });
