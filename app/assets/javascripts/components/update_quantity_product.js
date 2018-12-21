7eabafac780f4eb0eb479d98802811ba3fb2ca1f
frediansimanjuntak <fredian.pc@gmail.com>
2018-12-21 16:37:21 +0700

    add component js to update quantity item order

---
+++ app/assets/javascripts/components/update_quantity_product.js
@@ -0,0 +1,25 @@
+function update_quantity(variant_id, quantity) {
+  $.ajax({
+    url: "/orders/populate",
+    data: "quantity="+quantity+"&variant_id="+variant_id,
+    type:"post",
+    success:function( data ) {
+      console.log("result "+ data);
+    },
+    error:function( result ){ console.log(["error", result]); }
+  });
+}
+
+$(document).ready(function(){
+  $(".plus").click(function(){
+    var variant = $(this).attr('id').replace('button_plus', 'variant');
+    var variant_id = $('#'+variant).val();
+    update_quantity(variant_id, 1)
+  })
+
+  $(".min").click(function(){
+    var variant = $(this).attr('id').replace('button_min', 'variant');
+    var variant_id = $('#'+variant).val();
+    update_quantity(variant_id, -1)
+  })
+})
