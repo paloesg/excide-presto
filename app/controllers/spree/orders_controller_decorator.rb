75ef205a74417727837a8c95ea4487f823bdbb3e
frediansimanjuntak <fredian.pc@gmail.com>
2018-12-21 16:38:07 +0700

    modify populate function in order controller

--- app/controllers/spree/orders_controller_decorator.rb
+++ app/controllers/spree/orders_controller_decorator.rb
@@ -1,25 +1,39 @@
 Spree::OrdersController.class_eval do
   respond_override populate: { html: { success: lambda { render js: 'Spree.fetch_cart();$("#productContent").modal("hide")' } } }
 
+  def update
+    @variant = Spree::Variant.find(params[:variant_id]) if params[:variant_id]
+    if @order.contents.update_cart(order_params)
+      respond_with(@order) do |format|
+        format.html do
+          if params.key?(:checkout)
+            @order.next if @order.cart?
+            redirect_to checkout_state_path(@order.checkout_steps.first)
+          else
+            redirect_to cart_path
+          end
+        end
+      end
+    else
+      respond_with(@order)
+    end
+  end
+
   def populate
     order    = current_order(create_order_if_necessary: true)
     variant  = Spree::Variant.find(params[:variant_id])
     quantity = params[:quantity].to_i
     options  = params[:options] || {}
-    # render json: variant.to_json
 
-    # 2,147,483,647 is crazy. See issue #2695.
-    if quantity.between?(1, 2_147_483_647)
-      begin
-        order.contents.add(variant, quantity, options)
-        order.update_line_item_prices!
-        order.create_tax_charge!
-        order.update_with_updater!
-      rescue ActiveRecord::RecordInvalid => e
-        error = e.record.errors.full_messages.join(', ')
-      end
+    existing_quantity = current_order.line_items.find_by(variant_id: variant.id).quantity
+    input_quantity = existing_quantity + quantity
+    if input_quantity >= 0
+      order.contents.add(variant, quantity, options)
+      order.update_line_item_prices!
+      order.create_tax_charge!
+      order.update_with_updater!
     else
-      error = Spree.t(:please_enter_reasonable_quantity)
+      error = 'Quantity is not match with your order'
     end
 
     if error
