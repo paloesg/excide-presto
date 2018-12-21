5584cd54075045e86703167f09e910952e238cb8
Vicky Prahastra <vicky@excide.co>
2018-12-21 18:41:42 +0700

    Refactor display user email

--- app/controllers/spree/checkout_controller_decorator.rb
+++ app/controllers/spree/checkout_controller_decorator.rb
@@ -1,7 +1,6 @@
 Spree::CheckoutController.class_eval do
   def update
     @order = current_order(lock: true)
-    @user = @order.user
     if @order.update_from_params(params, permitted_checkout_attributes, request.headers.env)
       @order.temporary_address = !params[:save_user_address]
       unless @order.next
@@ -13,11 +12,11 @@ Spree::CheckoutController.class_eval do
         @current_order = nil
         admin_users = Spree::Role.find_by_name('admin').users
         admin_users.each do |admin|
-          OrderMailer.order_request(@order, @user, admin).deliver_now
+          OrderMailer.order_request(@order, admin).deliver_now
         end
         managers = Spree::Role.get_manager_by_department(current_store, @user)
         managers.each do |manager|
-          OrderMailer.order_request_to_manager(@order, @user, manager).deliver_now
+          OrderMailer.order_request_to_manager(@order, manager).deliver_now
         end
         flash.notice = Spree.t(:order_processed_successfully)
         flash['order_completed'] = true
--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -1,16 +1,14 @@
 class OrderMailer < ApplicationMailer
   layout 'mailer'
 
-  def order_request(order, user, admin)
+  def order_request(order, admin)
     sent_to = admin.email
-    @user = user
     @order = order
     mail to: sent_to, subject: Spree::Store.current.name + ' New Order Request'
   end
 
-  def order_request_to_manager(order, user, manager)
+  def order_request_to_manager(order, manager)
     sent_to = manager.email
-    @user = user
     @order = order
     mail to: sent_to, subject: Spree::Store.current.name + ' New Order Request - Waiting for Approval'
   end
