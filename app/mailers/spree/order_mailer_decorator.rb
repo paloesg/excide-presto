1b974631745bc7dd86c003f6a0d8b138025890c0
Vicky Prahastra <vicky@excide.co>
2018-12-23 00:06:11 +0700

    Change cancelation email subject

--- app/mailers/spree/order_mailer_decorator.rb
+++ app/mailers/spree/order_mailer_decorator.rb
@@ -2,14 +2,7 @@ Spree::OrderMailer.class_eval do
   def approve_email(order, resend = false)
     @order = order.respond_to?(:id) ? order : Spree::Order.find(order)
     subject = (resend ? "[#{Spree.t(:resend).upcase}] " : '')
-    subject += "#{Spree::Store.current.name} order approved ##{@order.number}"
-    mail(to: @order.email, from: from_address, subject: subject)
-  end
-
-  def approve_email(order, resend = false)
-    @order = order.respond_to?(:id) ? order : Spree::Order.find(order)
-    subject = (resend ? "[#{Spree.t(:resend).upcase}] " : '')
-    subject += "#{Spree::Store.current.name} order rejected ##{@order.number}"
+    subject += "#{Spree::Store.current.name} order Approved ##{@order.number}"
     mail(to: @order.email, from: from_address, subject: subject)
   end
 end
