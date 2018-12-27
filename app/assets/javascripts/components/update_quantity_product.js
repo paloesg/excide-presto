cdca78bd96d4bb8ad8aa13abf61493d5c457d54e
frediansimanjuntak <fredian.pc@gmail.com>
2018-12-27 15:25:23 +0700

    show popover increase/decrease quantity product

--- app/assets/javascripts/components/update_quantity_product.js
+++ app/assets/javascripts/components/update_quantity_product.js
@@ -10,19 +10,39 @@ function update_quantity(variant_id, quantity) {
   });
 }
 
-var update_data;
+$(document).on('mouseleave','.popover-content',function(){
+  $('[data-toggle="item-cart"]').popover('hide');
+});
 
-function start_timer_function(variant_id, quantity) {
-  update_data = setTimeout(function(){
-    update_quantity(variant_id, quantity);
-  }, 1000);
-}
-
-function stop_timer_function() {
-  clearTimeout(update_data);
-}
+$(document).on('click', function(e) {
+  $('[data-toggle="item-cart"]').each(function() {
+    if (!$(this).is(e.target) && $(this).has(e.target).length === 0 && $('.popover').has(e.target).length === 0) {
+      $(this).popover('hide');
+    }
+  });
+});
 
 $(document).ready(function (){
+  var update_data;
+
+  function start_timer_function(type, variant_id, quantity) {
+    $('[data-toggle="item-cart"]').popover('destroy');
+    update_data = setTimeout(function(){
+      var item_text = quantity <= 1 ? "item" : "items";
+      var type_text = type=='increase' ? "added" : "removed";
+      update_quantity(variant_id, type=='increase' ? quantity : -(quantity));
+      console.log(item_text, type_text)
+      $('[data-toggle="item-cart"]').popover({
+        html: true,
+        content: '<div class="content-popover"><div class="quantity col-md-2">'+quantity+'</div><div class="col-md-6">'+item_text +' '+type_text+'</div></div>',
+      }).popover('show');
+    }, 1000);
+  }
+
+  function stop_timer_function() {
+    clearTimeout(update_data);
+  }
+
   $(document).on('click', '.decrease,.increase', function() {
     var $btn = $(this);
     var count = ($btn.data("click_count") || 0) + 1;
@@ -35,11 +55,11 @@ $(document).ready(function (){
       is_add = $(this).hasClass('increase');
     if(is_add){
       $qty.val(current_val + 1);
-      start_timer_function($variant.val(), count);
+      start_timer_function('increase', $variant.val(), count);
     }
     else {
       $qty.val(current_val - 1);
-      start_timer_function($variant.val(), -(count));
+      start_timer_function('decrease', $variant.val(), count);
     }
 
     if($qty.val() == 0){
