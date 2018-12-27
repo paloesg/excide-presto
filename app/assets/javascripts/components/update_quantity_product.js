e7f87bfc7dc52dfb5a9fe8fa80feaebb228c31e5
frediansimanjuntak <fredian.pc@gmail.com>
2018-12-27 13:40:49 +0700

    set timer to increase/decrease quantity product

--- app/assets/javascripts/components/update_quantity_product.js
+++ app/assets/javascripts/components/update_quantity_product.js
@@ -10,20 +10,38 @@ function update_quantity(variant_id, quantity) {
   });
 }
 
+var update_data;
+
+function start_timer_function(variant_id, quantity) {
+  update_data = setTimeout(function(){
+    update_quantity(variant_id, quantity);
+  }, 1000);
+}
+
+function stop_timer_function() {
+  clearTimeout(update_data);
+}
+
 $(document).ready(function (){
   $(document).on('click', '.decrease,.increase', function() {
+    var $btn = $(this);
+    var count = ($btn.data("click_count") || 0) + 1;
+    $btn.data("click_count", count);
+    stop_timer_function();
+
     var $variant = $(this).closest('.quantity-input').find('.variant');
     var $qty = $(this).closest('.quantity-input').find('.quantity'),
       current_val = parseInt($qty.val()),
       is_add = $(this).hasClass('increase');
     if(is_add){
       $qty.val(current_val + 1);
-      update_quantity($variant.val(), 1)
+      start_timer_function($variant.val(), count);
     }
     else {
       $qty.val(current_val - 1);
-      update_quantity($variant.val(), -1)
+      start_timer_function($variant.val(), -(count));
     }
+
     if($qty.val() == 0){
       $('.inscrease_decrease[variant='+$variant.val()+']').hide();
       $('.add_to_cart[variant='+$variant.val()+']').show();
