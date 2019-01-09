3bde3e4b3a88a701e9077159b5b94bea9972102b
Vicky Prahastra <vicky@excide.co>
2019-01-09 10:46:51 +0700

    Send email after order state is awaiting approval

--- app/controllers/spree/checkout_controller_decorator.rb
+++ app/controllers/spree/checkout_controller_decorator.rb
@@ -1,6 +1,5 @@
 Spree::CheckoutController.class_eval do
   def update
-    @order = current_order(lock: true)
     if @order.update_from_params(params, permitted_checkout_attributes, request.headers.env)
       @order.temporary_address = !params[:save_user_address]
       unless @order.next
@@ -10,21 +9,14 @@ Spree::CheckoutController.class_eval do
 
       if @order.completed?
         @current_order = nil
-        admin_users = Spree::Role.find_by_name('admin').users
-        admin_users.each do |admin|
-          OrderMailer.order_request(@order, admin).deliver_now
-        end
-        managers = Spree::Role.get_manager_by_department(current_store, @order.user)
-        managers.each do |manager|
-          OrderMailer.order_request_to_manager(@order, manager).deliver_now
-        end
         flash.notice = Spree.t(:order_processed_successfully)
         flash['order_completed'] = true
         redirect_to completion_route
         @order.approved_by(@order.user) if managers.empty? or @order.user.has_spree_role? :manager
       elsif @order.awaiting_approval?
-        flash.notice = 'Your order has been processed successfully, awaiting for approval'
+        send_email_to_manager
         @order.finalize!
+        flash.notice = 'Your order has been processed successfully, awaiting for approval'
         redirect_to completion_route
       else
         redirect_to checkout_state_path(@order.state)
@@ -34,4 +26,16 @@ Spree::CheckoutController.class_eval do
     end
   end
 
+  private
+
+  def send_email_to_manager
+    managers = Spree::Role.get_manager_by_department(current_store, @order.user)
+    managers.each do |manager|
+      OrderMailer.order_request_to_manager(@order, manager).deliver_now
+    end
+  end
+
+  def set_order
+    @order = current_order(lock: true)
+  end
 end
