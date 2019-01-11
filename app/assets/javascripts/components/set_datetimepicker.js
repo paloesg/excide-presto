958a0f31ed8c1f90eb9fce045f530c90f6901325
Vicky Prahastra <vicky@excide.co>
2019-01-11 17:39:38 +0700

    Refactor naming variable

--- app/assets/javascripts/components/set_datetimepicker.js
+++ app/assets/javascripts/components/set_datetimepicker.js
@@ -1,13 +1,14 @@
 $(document).on('turbolinks:load', function () {
   var tomorrow = new Date();
+  var nextWeek = new Date();
   tomorrow.setDate(tomorrow.getDate()+1);
-  var date = new Date();
-  date.setDate(date.getDate()+7);
+  nextWeek.setDate(nextWeek.getDate()+7);
+
   $('.cart-datetime-picker').datetimepicker( {
     stepping: 60,
     enabledHours: [9, 10, 11, 12, 13, 14, 15, 16, 17],
     format: 'DD-MM-YYYY',
-    minDate: date
+    minDate: nextWeek
   });
   $('.cart-datetime-picker').val('');
   $('.cart-datetime-picker').attr("placeholder","DD-MM-YYYY");
