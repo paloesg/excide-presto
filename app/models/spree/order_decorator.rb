21d37c3234fe76d47b05358f7885def78372d129
Vicky Prahastra <vicky@excide.co>
2019-01-24 13:44:07 +0700

    Add rejected state order

--- app/models/spree/order_decorator.rb
+++ app/models/spree/order_decorator.rb
@@ -3,6 +3,7 @@ Spree::Order.class_eval do
     go_to_state :address
     go_to_state :confirm, if: ->(order) { order.confirmation_required? }
     go_to_state :awaiting_approval
+    go_to_state :rejected
   end
 
   def self.department(current_user)
