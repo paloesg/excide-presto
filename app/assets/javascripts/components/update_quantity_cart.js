3432bb262f99efb6667a95e8cb30a155745727c5
Vicky Prahastra <vicky@excide.co>
2019-01-25 10:46:35 +0700

    Increase and decrease quantity items on edit rejected order

--- app/assets/javascripts/components/update_quantity_cart.js
+++ app/assets/javascripts/components/update_quantity_cart.js
@@ -1,7 +1,8 @@
-function update_quantity_cart(variant_id, quantity) {
+function update_quantity_cart(variant_id, quantity, order_id = null) {
+  console.log(variant_id, quantity, order_id);
   $.ajax({
     url: "/orders/populate",
-    data: "quantity="+quantity+"&variant_id="+variant_id,
+    data: "quantity="+quantity+"&variant_id="+variant_id+"&order_id="+order_id,
     type:"post",
     success:function( data ) {
       location.reload();
@@ -16,12 +17,13 @@ $(document).on('mouseleave','.popover-content',function(){
 
 $(document).ready(function (){
   var update_data;
+  var order_id = $('#order_id').val();
   function start_timer_function(type, variant_id, quantity) {
     $('[data-toggle="item-cart"]').popover('destroy');
     update_data = setTimeout(function(){
       var item_text = quantity <= 1 ? "item" : "items";
       var type_text = type=='increase' ? "added" : "removed";
-      update_quantity_cart(variant_id, type=='increase' ? quantity : -(quantity));
+      update_quantity_cart(variant_id, type=='increase' ? quantity : -(quantity), order_id);
 
       $('.decrease-quantity').data("click_count", 0)
       $('.increase-quantity').data("click_count", 0)
