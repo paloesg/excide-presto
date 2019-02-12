e603bfda0aed2e24dd81ee88b50665241ab6d970
Vicky Prahastra <vicky@excide.co>
2019-02-12 11:39:39 +0700

    Fix cannot send email to manager

--- app/mailers/spree/order_mailer_decorator.rb
+++ app/mailers/spree/order_mailer_decorator.rb
@@ -1,7 +1,7 @@
 Spree::OrderMailer.class_eval do
   def confirm_email(order, resend = false)
     @order = order.respond_to?(:id) ? order : Spree::Order.find(order)
-    @managers = Spree::Role.get_manager_by_department(Spree::Store.current, @order.user)
+    @managers = Spree::Role.get_manager_by_department(@order.user)
     subject = (resend ? "[#{Spree.t(:resend).upcase}] " : '')
     subject += "#{Spree::Store.current.name} your Order ##{@order.number} is waiting for Approval from Manager"
     mail(to: @order.email, from: from_address, subject: subject)
