0993a9dea38ea5c6fe613c6bc755069c42b66745
Han-Sheng Chin <hschin@gmail.com>
2019-02-13 22:53:51 +0800

    Refactor code for sending mailer to be more organised

--- app/mailers/spree/order_mailer_decorator.rb
+++ app/mailers/spree/order_mailer_decorator.rb
@@ -1,32 +1,36 @@
 Spree::OrderMailer.class_eval do
-  # sent email to user who placed the order with the details of order.
+  # Send email to user who placed the order with the details of order.
   def confirm_order(order, resend = false)
     @order = order.respond_to?(:id) ? order : Spree::Order.find(order)
     @managers = Spree::Role.get_manager_by_department(@order.user)
     subject = (resend ? "[#{Spree.t(:resend).upcase}] " : '')
-    subject += "#{Spree::Store.current.name} your Order ##{@order.number} is waiting for Approval from Manager"
+    subject += "#{Spree::Store.current.name} | Your order ##{@order.number} is pending approval"
+
     mail(to: @order.email, from: from_address, subject: subject)
   end
 
-  # sent email to user the order is approved
+  # Send email to managers for new order that is awaiting approval
+  def request_approval_to_manager(order, manager)
+    @order = order.respond_to?(:id) ? order : Spree::Order.find(order)
+    subject = "#{Spree::Store.current.name} | Order ##{@order.number} requires your approval"
+
+    mail(to: manager.email, from: from_address, subject: subject)
+  end
+
+  # Send email to user after the order is approved
   def order_approved(order, resend = false)
     @order = order.respond_to?(:id) ? order : Spree::Order.find(order)
     subject = (resend ? "[#{Spree.t(:resend).upcase}] " : '')
-    subject += "#{Spree::Store.current.name} order Approved ##{@order.number}"
+    subject += "#{Spree::Store.current.name} | Your order ##{@order.number} has been approved"
+
     mail(to: @order.email, from: from_address, subject: subject)
   end
 
-  # sent email to managers for new request order is awaiting approval from them
-  def request_approval_to_manager(order, manager)
-    sent_to = manager.email
-    @order = order
-    mail to: sent_to, from: from_address, subject: Spree::Store.current.name + ' New Order Request - Awaiting Your Approval'
-  end
+  # Send email to admin for an order that has been approved by manager
+  def order_notify_admin(order, admin)
+    @order = order.respond_to?(:id) ? order : Spree::Order.find(order)
+    subject = "#{Spree::Store.current.name} | An order ##{@order.number} has been received"
 
-  # sent email to admin to inform an order is approved by manager
-  def confirm_order_approved(order, admin)
-    sent_to = admin.email
-    @order = order
-    mail to: sent_to, from: from_address, subject: Spree::Store.current.name + " order Approved ##{@order.number}"
+    mail(to: admin.email, from: from_address, subject: subject)
   end
 end
