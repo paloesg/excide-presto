d2c4da78efbd5b04472dfa35ef6ecfbec67e791c
frediansimanjuntak <fredian.pc@gmail.com>
2019-02-14 15:13:10 +0700

    add state delivery when checkout to create shipment state

--- app/models/spree/order_decorator.rb
+++ app/models/spree/order_decorator.rb
@@ -1,11 +1,13 @@
 Spree::Order.class_eval do
   checkout_flow do
     go_to_state :address
+    go_to_state :delivery
     go_to_state :preview
     go_to_state :confirm, if: ->(order) { order.confirmation_required? }
     go_to_state :awaiting_approval
     go_to_state :rejected
     go_to_state :complete
+    remove_transition from: :delivery, to: :confirm
   end
 
   # Update order state and approved by user
