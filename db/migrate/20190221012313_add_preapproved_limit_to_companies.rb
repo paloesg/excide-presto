c6893dbd6ecac0be7f8ae2941bc2e12dcec93a38
Han-Sheng Chin <hschin@gmail.com>
2019-02-21 17:04:17 +0800

    Fix migration name

--- db/migrate/20190221012313_add_preapproved_limit_to_companies.rb
+++ db/migrate/20190221012313_add_preapproved_limit_to_companies.rb
@@ -1,4 +1,4 @@
-class AddPreapprovedLimittoCompanies < ActiveRecord::Migration[5.2]
+class AddPreapprovedLimitToCompanies < ActiveRecord::Migration[5.2]
   def change
     add_column :spree_companies, :preapproved_limit, :decimal, precision: 10, scale: 2
   end
