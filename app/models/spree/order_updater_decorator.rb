f1db9d86b6b0cc785613f7b2f3b48bd54f8ea351
frediansimanjuntak <fredian.pc@gmail.com>
2019-02-26 16:36:16 +0700

    remove unused file and function

--- app/models/spree/order_updater_decorator.rb
+++ app/models/spree/order_updater_decorator.rb
@@ -15,30 +15,6 @@ Spree::OrderUpdater.class_eval do
     persist_totals
   end
 
-  def update_shipment_state
-    if order.backordered?
-      order.shipment_state = 'backorder'
-    else
-      # get all the shipment states for this order
-      shipment_states = shipments.states
-      order.shipment_state = if shipment_states.size > 1
-        # multiple shiment states means it's most likely partially shipped
-                               'partial'
-                             else
-        # will return nil if no shipments are found
-                                shipment_states.first
-        # TODO: inventory unit states?
-        # if order.shipment_state && order.inventory_units.where(shipment_id: nil).exists?
-        #   shipments exist but there are unassigned inventory units
-        #   order.shipment_state = 'partial'
-        # end
-                             end
-    end
-
-    order.state_changed('shipment')
-    order.shipment_state
-  end
-
   def update_payment_state
     last_state = order.payment_state
     if payments.present? && payments.valid.empty?
