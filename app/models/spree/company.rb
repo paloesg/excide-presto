cdfadf94933aafdbe57d23921341381269160967
Han-Sheng Chin <hschin@gmail.com>
2019-03-13 10:41:11 +0800

    Rename method for clarity

--- app/models/spree/company.rb
+++ app/models/spree/company.rb
@@ -4,13 +4,17 @@ class Spree::Company < Spree::Base
   has_one :store
   has_one :role
   belongs_to :company_address, foreign_key: :address_id, class_name: 'Spree::Address', optional: true
+
   validates :name, presence: true
   validates :description, presence: true
-  after_create :create_role
+
+  after_create :create_manager
 
   accepts_nested_attributes_for :company_address
 
-  def create_role
+  private
+
+  def create_manager
     role = Spree::Role.new(name: 'manager', company_id: self.id)
     role.save
   end
