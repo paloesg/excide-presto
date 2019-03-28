53660272ce53ef4655813c1ef0494a8667464aba
Han-Sheng Chin <hschin@gmail.com>
2019-03-28 17:31:37 +0800

    Remove unneeded migration files

--- db/migrate/20190318030050_remove_date_in_department_budget.rb
+++
@@ -1,7 +0,0 @@
-class RemoveDateInDepartmentBudget < ActiveRecord::Migration[5.2]
-  def change
-    remove_column :spree_department_budgets, :start_date, :timestamp
-    remove_column :spree_department_budgets, :end_date, :timestamp
-    remove_column :spree_department_budgets, :total_order_price, :decimal
-  end
-end
--- db/migrate/20190320031208_drop_department_budgets.rb
+++
@@ -1,5 +0,0 @@
-class DropDepartmentBudgets < ActiveRecord::Migration[5.2]
-  def change
-    drop_table :spree_department_budgets
-  end
-end
