4b127afe3c5e392bdb3b9eb86e8d3aed2135d60f
frediansimanjuntak <fredian.pc@gmail.com>
2019-04-02 13:40:41 +0700

    Fix view form user on admin user page

--- app/assets/javascripts/components/get_departments_by_company.js
+++ app/assets/javascripts/components/get_departments_by_company.js
@@ -9,7 +9,7 @@ function getDepartments(company){
       }
       $("#user_department_id").empty();
       $("#user_department_id").select2({
-        include_blank: false
+        include_blank: "None"
       });
       $.each(departments, function(index,department){
         $("#user_department_id").append($("<option></option>", {"value":department.id, "text":department.name}));
