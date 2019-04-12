d4886712cd587c4a0732b92b682b722e56513067
frediansimanjuntak <fredian.pc@gmail.com>
2019-04-12 16:56:30 +0700

    Change design services page
    - Fix javascript to show service modal
    - Remove unused function and file

--- app/assets/javascripts/spree/frontend/show_service.js
+++ app/assets/javascripts/spree/frontend/show_service.js
@@ -16,37 +16,7 @@ function showServiceForm(url, fields) {
   markup.formRender({ formData });
   form_service = markup.formRender("html");
 
-
-  // Load Service Content
-  $("#modalContent").load(window.location.origin +'/'+ url, function(response, status, xhr) {
-    if (status == "success") {
-      $('.loader').hide();
-      $('.modal-content').show();
-
-    }
-    // Load Js for switching the service main image by hovering the thumbnails
-    var thumbnails = $("#service-images").next();
-    ($("#main-image")).data("selectedThumb", ($("#main-image img")).attr("src"));
-    thumbnails.find("li").eq(0).addClass("selected");
-    thumbnails.find("a").on("click", function (event) {
-      ($("#main-image")).data("selectedThumb", ($(event.currentTarget)).attr("href"));
-      ($("#main-image")).data("selectedThumbId", ($(event.currentTarget)).parent().attr("id"));
-      thumbnails.find("li").removeClass("selected");
-      ($(event.currentTarget)).parent("li").addClass("selected");
-      return false;
-    });
-    thumbnails.find("li").on("mouseenter", function (event) {
-      return ($("#main-image img")).attr("src", ($(event.currentTarget)).find("a").attr("href"));
-    });
-    thumbnails.find("li").on("mouseleave", function (event) {
-      return ($("#main-image img")).attr("src", ($("#main-image")).data("selectedThumb"));
-    });
-    $( ".form_service" ).append( form_service );
-    set_datetimepicker_format()
-  });
-  $('#serviceContent').on('hidden.bs.modal', function (e) {
-    $('.loader').show();
-    $('.modal-content').hide();
-    $('#serviceContent').find('.modal-body').html('<div class="col-xs-offset-5"><div class="loader"></div></div>');
-  })
+  $( ".form-service" ).empty();
+  $( ".form-service" ).append( form_service );
+  set_datetimepicker_format();
 }
