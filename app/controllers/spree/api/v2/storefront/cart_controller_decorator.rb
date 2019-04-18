a66c2a656aa8fa779d20cb810a4b49118adc64eb
frediansimanjuntak <fredian.pc@gmail.com>
2019-04-18 10:50:13 +0700

    Revert "Fix set remaining budget to 0 when user don't have department or company"
    This reverts commit c83ce55ff0b48e745d1e72446bea4c84efaef628.

--- app/controllers/spree/api/v2/storefront/cart_controller_decorator.rb
+++ app/controllers/spree/api/v2/storefront/cart_controller_decorator.rb
@@ -8,9 +8,6 @@ Spree::Api::V2::Storefront::CartController.class_eval do
       currency: current_currency
     }
 
-    return render_error_user_department_company if spree_current_user.company.blank? && spree_current_user.department.blank?
-    return render_error_default_currency if current_currency.blank?
-
     order   = spree_current_order if spree_current_order.present?
     order ||= create_service.call(order_params).value
 
@@ -27,13 +24,5 @@ Spree::Api::V2::Storefront::CartController.class_eval do
     )
 
   render_order(result)
-  end
-
-  def render_error_user_department_company
-    render json: { error: 'Status user department and company undefined' }, status: 422
-  end
-
-  def render_error_default_currency
-    render json: { error: 'Status currency undefined' }, status: 422
   end
 end
