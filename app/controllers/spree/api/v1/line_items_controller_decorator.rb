da902f91c943453fd03b565b4079866693959019
Vicky Prahastra <vicky@excide.co>
2019-04-24 21:41:06 +0700

    Refactor code

--- app/controllers/spree/api/v1/line_items_controller_decorator.rb
+++ app/controllers/spree/api/v1/line_items_controller_decorator.rb
@@ -2,7 +2,6 @@ Spree::Api::V1::LineItemsController.class_eval do
   after_action :generate_purchase_order
 
   def generate_purchase_order
-    @order = order
-    GeneratePurchaseOrderJob.perform_later(@order)
+    GeneratePurchaseOrderJob.perform_later(order)
   end
 end
