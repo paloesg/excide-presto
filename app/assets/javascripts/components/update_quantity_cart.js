2f33ca1a2ed18da5aa90c00a5a03ca684c6ed769
frediansimanjuntak <fredian.pc@gmail.com>
2019-04-24 12:15:28 +0700

    Fix js add cart and reorder

--- app/assets/javascripts/components/update_quantity_cart.js
+++ app/assets/javascripts/components/update_quantity_cart.js
@@ -1,21 +1,34 @@
 // If the order number is null it will update the order in cart
-function update_quantity_cart(variant_id, quantity, order_number = null, item_text = null, type_text = null) {
+function update_quantity_cart(body_id, variant_id, quantity, item_text = null, type_text = null) {
+
+  var order_number = $('#order_number').val();
+  var order_id = $('#order_id').val();
+  console.log(order_number, order_id, body_id)
   $.ajax({
     url: "/orders/populate",
     data: "quantity="+quantity+"&variant_id="+variant_id+"&order_number="+order_number,
     type:"post",
     success:function( data ) {
-      if (item_text && type_text) {
-        $('[data-toggle="item-cart"]').attr('data-content', '<div class="content-popover"><div class="quantity col-md-2">'+Math.abs(quantity)+'</div><div class="col-md-6">'+item_text +' '+type_text+'</div></div>');
-        $('[data-toggle="item-cart"]').popover('show');
+      if (body_id == 'cart'){
+        if (item_text && type_text) {
+          $('[data-toggle="item-cart"]').attr('data-content', '<div class="content-popover"><div class="quantity col-md-2">'+Math.abs(quantity)+'</div><div class="col-md-6">'+item_text +' '+type_text+'</div></div>');
+          $('[data-toggle="item-cart"]').popover('show');
+        }
+        refresh_cart_partial();
+      }
+      else {
+        refresh_reorder_partial(data.order.id, order_number);
       }
     },
     error:function( err ){
-      $('[data-toggle="item-cart"]').attr('data-content', '<div class="content-popover">Error adding to cart</div>');
-      $('[data-toggle="item-cart"]').popover('show');
-    },
-    complete:function(){
-      refresh_cart_partial();
+      if (body_id == 'cart'){
+        $('[data-toggle="item-cart"]').attr('data-content', '<div class="content-popover">Error adding to cart</div>');
+        $('[data-toggle="item-cart"]').popover('show');
+        refresh_cart_partial();
+      }
+      else {
+        refresh_reorder_partial(order_id, order_number);
+      }
     }
   });
 }
@@ -26,23 +39,28 @@ function refresh_cart_partial() {
   })
 }
 
+function refresh_reorder_partial(order_id, order_number) {
+  $.ajax({
+    url: "/reorder_partial/"+order_id+"/"+order_number
+  })
+}
+
 $(document).on('mouseleave','.popover-content',function(){
   $('[data-toggle="item-cart"]').popover('hide');
 });
 
 $(document).ready(function (){
   var update_data;
-  var order_number = $('#order_number').val();
   function start_timer_function(type, variant_id, quantity) {
     $('[data-toggle="item-cart"]').popover('destroy');
     update_data = setTimeout(function(){
       var item_text = quantity <= 1 ? "item" : "items";
       var type_text = type=='increase' ? "added" : "removed";
-      update_quantity_cart(variant_id, type=='increase' ? quantity : -(quantity), order_number, item_text, type_text);
+      var body_id = $('#body_id').val();
+      update_quantity_cart(body_id, variant_id, type=='increase' ? quantity : -(quantity), item_text, type_text);
 
       $('.decrease-quantity').data("click_count", 0)
       $('.increase-quantity').data("click_count", 0)
-
 
       $('[data-toggle="item-cart"]').popover({
         html: true,
@@ -59,8 +77,8 @@ $(document).ready(function (){
   $(document).on('click', '.delete_line_item', function() {
     var variant = $(this).closest('tr').find('.variant').val();
     var quantity = $(this).closest('tr').find('.line_item_quantity');
-    var order_number = $('#order_number').val();
-    update_quantity_cart(variant, -(quantity.val()), order_number);
+    var body_id = $('#body_id').val();
+    update_quantity_cart(body_id, variant, -(quantity.val()));
     quantity.val(0);
   })
 
