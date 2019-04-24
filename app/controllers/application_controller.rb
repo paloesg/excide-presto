7c02be9eb30791444c4021da7e83e711f09f73fd
Vicky Prahastra <vicky@excide.co>
2019-04-24 11:30:20 +0700

    Redirect to default store if user doesnt have company

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,17 +7,26 @@ class ApplicationController < ActionController::Base
     spree_current_user != nil
   end
 
+  def default_store
+    Spree::Store.find_by_default(true)
+  end
+
   def redirect_to_user_company_store
     # Check user company domain and domain is match with current site domain
     if spree_current_user&.company&.store&.url && spree_current_user&.company&.store&.url != request.host
       unless spree_current_user.has_spree_role?('admin') && params[:controller].include?('spree/admin')
-        redirect_to "http://#{spree_current_user.company.store.url}:3000"
+        redirect_to "http://#{spree_current_user.company&.store&.url}:3000"
       end
+    elsif spree_current_user&.company.nil? && default_store.url.lines.first != request.host
+      redirect_to "http://#{default_store.url.lines.first}:3000"
     end
   end
 
   def require_login
-    redirect_to spree_login_path unless logged_in?
-    redirect_to_user_company_store
+    unless logged_in?
+      redirect_to spree_login_path
+    else
+      redirect_to_user_company_store
+    end
   end
 end
