9e5b78938aeab7d221b7362882e20fe7d999cc9f
Vicky Prahastra <vicky@excide.co>
2019-05-06 15:01:06 +0700

    Add department id to new role manager

--- app/assets/javascripts/components/update_manager_role.js
+++ app/assets/javascripts/components/update_manager_role.js
@@ -1,7 +1,7 @@
-function update_role(user_id, company_id, is_manager) {
+function update_role(user_id, company_id, department_id, is_manager) {
   $.ajax({
     url: "/admin/users/"+user_id+"/update/role",
-    data: "company_id="+company_id+"&type_manager="+is_manager,
+    data: "company_id="+company_id+"&department_id="+department_id+"&type_manager="+is_manager,
     type:"put",
     success:function( data ) {
 
@@ -14,10 +14,11 @@ $(document).ready(function(){
   $('.role-check').on("change", function() {
     var $user_id = $(this).closest('.role-form').find('.user-id');
     var $company_id = $(this).closest('.role-form').find('.company-id');
+    var $department_id = $(this).closest('.role-form').find('.department-id');
     if($(this).is(":checked")) {
-      update_role($user_id.val(), $company_id.val(), "set_manager");
+      update_role($user_id.val(), $company_id.val(), $department_id.val(), "set_manager");
     } else {
-      update_role($user_id.val(), $company_id.val(), "unset_manager");
+      update_role($user_id.val(), $company_id.val(), $department_id.val(), "unset_manager");
     }
   })
 })
