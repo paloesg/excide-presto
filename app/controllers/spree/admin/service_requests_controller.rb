52a22a876cd0884e7bbf27724c58922f892dc747
Han-Sheng Chin <hschin@gmail.com>
2019-05-10 16:40:44 +0800

    Update processed by user if service request is rejected

--- app/controllers/spree/admin/service_requests_controller.rb
+++ app/controllers/spree/admin/service_requests_controller.rb
@@ -30,6 +30,7 @@ module Spree
       def reject
         @service_request.rejected!
         @service_request.updated_by(try_spree_current_user)
+        @service_request.process_by(try_spree_current_user)
         flash[:success] = Spree.t(:service_request_rejected)
         redirect_back fallback_location: spree.edit_admin_service_request_url(@service_request)
       end
