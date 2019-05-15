409494f7d4bd1b765a6c2a435b5e1f1e697a8b7f
frediansimanjuntak <fredian.pc@gmail.com>
2019-05-15 15:16:22 +0700

    Move css and js files, and fix rendering that files

---
+++ app/assets/javascripts/spree/custom-frontend.js
@@ -0,0 +1,9 @@
+
+//= require spree/frontend/component_function
+//= require spree/frontend/show_service
+//= require spree/frontend/modal_product
+//= require spree/frontend/popover_cart
+//= require spree/frontend/update_quantity_product
+//= require spree/frontend/update_quantity_cart
+
+//= require spree/frontend/foundry/index
