fb6daea55540ae2c6b81d5ce544cdb1f711318e4
frediansimanjuntak <fredian.pc@gmail.com>
2019-10-14 12:12:14 +0700

    Refactor calculate budget used add order with awaiting approval

--- app/models/spree/department.rb
+++ app/models/spree/department.rb
@@ -9,7 +9,14 @@ class Spree::Department < Spree::Base
   after_create :create_manager
 
   def budget_used
-    budget_used = Spree::Order.includes(:user).where(spree_orders: {state: 'complete'} ).where(spree_users: {department_id: self.id}).where('spree_orders.approved_at > ? AND spree_orders.approved_at < ?', Time.current.beginning_of_month, Time.current.end_of_month).sum(:total)
+    #calculate total who have status awaiting approval and updated_at in current month
+    budget_awaiting_approval= Spree::Order.includes(:user).where(spree_orders: {state: 'awaiting_approval'} ).where(spree_users: {department_id: self.id}).where('spree_orders.updated_at > ? AND spree_orders.updated_at < ?', Time.current.beginning_of_month, Time.current.end_of_month).sum(:total)
+
+    #calculate total who have status completed and approved_at in current month
+    budget_completed = Spree::Order.includes(:user).where(spree_orders: {state: 'complete'} ).where(spree_users: {department_id: self.id}).where('spree_orders.approved_at > ? AND spree_orders.approved_at < ?', Time.current.beginning_of_month, Time.current.end_of_month).sum(:total)
+
+    #calculate total awaiting approval and total completed
+    budget_used = budget_awaiting_approval + budget_completed
     budget_used || 0
   end
 
