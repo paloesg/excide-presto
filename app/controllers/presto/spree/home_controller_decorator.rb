143f9a3b1e3d0a3fd353dc8a77b43c1697710b65
Vicky Prahastra <vicky@excide.co>
2020-01-08 10:49:48 +0700

    Fix class in home controller decorator to fix index page

--- app/controllers/presto/spree/home_controller_decorator.rb
+++ app/controllers/presto/spree/home_controller_decorator.rb
@@ -1,14 +1,16 @@
 module Presto
   module Spree
-    module HomeController
+    module HomeControllerDecorator
       def self.prepended(base)
-        before_action :set_categories
+        base.before_action :set_categories
       end
 
       def set_categories
         # Combine products and services
-        @categories = Spree::Taxonomy.find_by_name('Categories').root.children.take(3) + Spree::Taxonomy.find_by_name('Services').root.children.all()
+        @categories = ::Spree::Taxonomy.find_by_name('Categories').root.children.take(3) + ::Spree::Taxonomy.find_by_name('Services').root.children.all()
       end
     end
   end
 end
+
+::Spree::HomeController.prepend Presto::Spree::HomeControllerDecorator
